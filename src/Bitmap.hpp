c693f75da2a6eb11554b667bd23a89a27a061b79
Sachith Dunatunga <neocpp@gmail.com>
2015-09-30 01:31:00 -0400

    retabbed bitmap.hpp

--- src/Bitmap.hpp
+++ src/Bitmap.hpp
@@ -7,36 +7,37 @@
 namespace SimpleRaytracer {
 
 class Bitmap
-{	public:
-		Bitmap(int x, int y);
+{
+    public:
+        Bitmap(int x, int y);
 
-		const int width() const { return width_; }
-		const int height() const { return height_; }
+        const int width() const { return width_; }
+        const int height() const { return height_; }
 
-		void set_pixel(int x, int y, const RGBColor &c);
-		RGBColor get_pixel(int x, int y) const;
+        void set_pixel(int x, int y, const RGBColor &c);
+        RGBColor get_pixel(int x, int y) const;
 
         /*
-            Be careful with this, it actually will fail if we don't run this on a
-            little-endian machine, since BMP format uses little endian.
+           Be careful with this, it actually will fail if we don't run this on a
+           little-endian machine, since BMP format uses little endian.
 
-            The only way you'll know is that the bitmap won't have the magic header
-            'BM' at the beginning of the file.
+           The only way you'll know is that the bitmap won't have the magic header
+           'BM' at the beginning of the file.
 
-            I haven't thought of a good way to fix this yet.
-        */
-		void write_file(const char *file) const;
+           I haven't thought of a good way to fix this yet.
+         */
+        void write_file(const char *file) const;
 
-	private:
+    private:
         const int width_;
         const int height_;
 
-		std::vector<b24bitpixel_t> data_;
+        std::vector<b24bitpixel_t> data_;
 
-		bfheader_t bf_;
-		biheader_t bi_;
-		int datasize_;
-		int filesize_;
+        bfheader_t bf_;
+        biheader_t bi_;
+        int datasize_;
+        int filesize_;
 };
 
 } // namespace SimpleRaytracer
